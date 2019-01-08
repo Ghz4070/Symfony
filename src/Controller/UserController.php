a4745edc3d0ca25e4285da54ec6b22d5a9b7adf2
Ghz4070 <ghezal-ilies@outlook.fr>
2019-01-08 11:08:12 +0100

    UserModif

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -10,10 +13,20 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="user")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // $this->redirectToRoute(‘register_sucess’);
+        }
+
+        return $this->render('user/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
